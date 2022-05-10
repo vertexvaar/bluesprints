6fac769135bea4000119ef225c17227cb4c65e65
Oliver Eglseder <php@vxvr.de>
2022-05-10 22:29:12 +0200

    [REFACTOR] Replace nested dirname calls with secondary argument

--- packages/bluesprints/resources/public/index.php
+++ packages/bluesprints/resources/public/index.php
@@ -7,7 +7,7 @@ use VerteXVaaR\BlueSprints\Http\Application;
 use VerteXVaaR\BlueSprints\Utility\Error;
 
 if (!defined('VXVR_BS_ROOT')) {
-    define('VXVR_BS_ROOT', dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR);
+    define('VXVR_BS_ROOT', dirname(__DIR__, 2) . DIRECTORY_SEPARATOR);
 }
 
 if (!class_exists(ClassLoader::class, false)) {
